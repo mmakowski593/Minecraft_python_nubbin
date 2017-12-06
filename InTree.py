07e9f45fc762b334526cac4ebfde4db1606a9ed9
mmakowski593 <33494031+mmakowski593@users.noreply.github.com>
2017-12-06 10:58:34 -0600

    Create InTree.py

---
+++ InTree.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+pos = mc.player.getPos()
+x = pos.x
+y = pos.y
+z = pos.z
+
+blockType = mc.getBlock(x, y - 1, z)
+if blockType == 11 or blockType == 18 :
+    mc.postToChat("birdwatching is ma lifeeee")
+else :
+    mc.postToChat("on the ground dude")
